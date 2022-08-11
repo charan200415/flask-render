627497a4fcb1133d7e0100f15373db49027a5a1d
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-08-11 11:31:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/", subdomain="<username>")  # now the subdomain will be passed into the parameter 'username'
+def profile(username):
+    return "Hello " + username + "!"
